b93a7e34d7463c3de4e4ecf2897b69767aafc20f
Your Name <you@example.com>
2024-09-07 17:34:07 +0200

    modules more

--- modules/shared/configuration.nix
+++ modules/shared/configuration.nix
@@ -22,36 +22,6 @@
   ];
 
 
-
-  # Select internationalisation properties.
-  #};
-  i18n.defaultLocale = "en_US.UTF-8";
-  #console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "pl";
-  #   useXkbConfig = true; # use xkb.options in tty.
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
   #Define a user account. Don't forget to set a password with `passwd`.
   users.users.rafal = {
     isNormalUser = true;
@@ -78,21 +48,6 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
--- modules/shared/system.nix
+++ modules/shared/system.nix
@@ -1,4 +1,14 @@
 {
   # Set your time zone.
   time.timeZone = "Europe/Warsaw";
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+
+  # Select internationalisation properties.
+  #};
+  i18n.defaultLocale = "en_US.UTF-8";
 }
