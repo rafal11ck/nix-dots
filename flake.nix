dd146d1a50a890785e60f79d064c751684a31d6f
rafal11ck <76795799+rafal11ck@users.noreply.github.com>
2024-09-09 21:09:52 +0200

    merge flake

--- flake.nix
+++ flake.nix
@@ -11,8 +11,19 @@
   };
 
   outputs =
-    { self, nixpkgs, home-manager, ... }@inputs:
     {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+
       nixosConfigurations.default = nixpkgs.lib.nixosSystem {
         specialArgs = {
           inherit inputs;
@@ -20,13 +31,25 @@
         modules = [
           ./modules/shared/default.nix
           /etc/nixos/hardware-configuration.nix
-         home-manager.nixosModules.home-manager
-	 {
-	  home-manager.useGlobalPkgs = true;
-	  home-manager.useUserPackages = true;
-	  home-manager.users.rafal = import ./home/home.nix;
-	 }
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.rafal = import ./home/home.nix;
+          }
         ];
       };
+      # For home-manager standalone install. NixOS-less home manager.  
+      homeConfigurations."rafal" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home/home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+
     };
 }
