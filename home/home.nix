4434c03c7f88bc3fae03eed9dc7a790a76b59b0a
Your Name <you@example.com>
2024-09-12 00:07:42 +0200

    try to modularize heloo

--- home/home.nix
+++ home/home.nix
@@ -5,40 +5,44 @@
   ...
 }:
 let
-  inherit (config) values;
   modulePath = ../modules;
 in
 {
 
-  imports = [ (modulePath + /shared/values.nix) ];
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  # Those are made with mkDefault so that if this home manager works standalone 
-  # but nixos won't error of duplicate.
-  # This config is for home not system so it has lower piriorty.
+  imports = [
+    (modulePath + /shared/values)
+    ./hello
+  ];
 
   home =
     let
-      username = "${values.mainUser}";
+      username = "${config.values.mainUser}";
     in
     {
-      inherit username;
-      homeDirectory = lib.mkDefault "/home/${username}";
-
-      stateVersion = lib.mkDefault "24.05"; # Please read the comment before changing.
 
       # The home.packages option allows you to install Nix packages into your
       # environment.
       packages = [
         # # Adds the 'hello' command to your environment. It prints a friendly
         # # "Hello, world!" when run.
-        pkgs.hello
         pkgs.git
         pkgs.neovim
         pkgs.gitui
         pkgs.tree
         pkgs.cowsay
       ];
+
+      # Home Manager needs a bit of information about you and the paths it should
+      # manage.
+      # Those are made with mkDefault so that if this home manager works standalone
+      # but nixos won't error of duplicate.
+      # This config is for home not system so it has lower piriorty.
+
+      inherit username;
+      homeDirectory = lib.mkDefault "/home/${username}";
+
+      stateVersion = lib.mkDefault "24.05"; # Please read the comment before changing.
+
     };
 
   # Let Home Manager install and manage itself.
