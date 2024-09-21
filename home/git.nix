e621c4f32ae79fbdba00707d92f5b4b03584bf91
rafal <you@example.com>
2024-09-21 21:55:57 +0200

    git config

--- home/git.nix
+++ home/git.nix
@@ -1,5 +1,14 @@
 {
+  config,
+  ...
+}:
+let
+  values = config.values;
+in
+{
   programs.git = {
     enable = true;
+    userName = "${values.mainUser}";
+    userEmail = "you@example.com";
   };
 }
