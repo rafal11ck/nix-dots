5e2ffda8954b46abcf3b9fde554b646d5faf7116
rafal <you@example.com>
2024-09-21 22:17:18 +0200

    jellyfin-mpv-shim mpv config

--- home/jellyfin-mpv-shim.nix
+++ home/jellyfin-mpv-shim.nix
@@ -4,18 +4,19 @@
   pkgs,
   ...
 }:
-
-let
-  scriptName = "jellyfin-mpv-shim-config-chose.sh";
-in
 {
   home.packages = with pkgs; [
     jellyfin-mpv-shim
   ];
 
-  xdg.configFile."scripts/${scriptName}" = {
-    source = "${config.values.dotfilesPath}" + "/scripts/.config/scripts/" + "${scriptName}";
+  xdg.configFile."scripts/jellyfin-mpv-shim-config-chose.sh" = {
+    source = "${config.values.dotfilesPath}" + "/scripts/.config/scripts/" + "jellyfin-mpv-shim-config-chose.sh";
   };
+
+  xdg.configFile."jellyfin-mpv-shim/mpv.conf" = {
+    source = "${config.values.dotfilesPath}" + "/jellyfin-mpv-shim/.config/jellyfin-mpv-shim/mpv.conf";
+  };
+
 
   home.sessionPath = [ (config.xdg.configHome + "/scripts") ];
 }
