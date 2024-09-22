935dd75fcd2e28cc9683f57f05510d73d2166118
rafal <you@example.com>
2024-09-22 11:08:37 +0200

    format

--- home/jellyfin-mpv-shim.nix
+++ home/jellyfin-mpv-shim.nix
@@ -10,13 +10,13 @@
   ];
 
   xdg.configFile."scripts/jellyfin-mpv-shim-config-chose.sh" = {
-    source = "${config.values.dotfilesPath}" + "/scripts/.config/scripts/" + "jellyfin-mpv-shim-config-chose.sh";
+    source =
+      "${config.values.dotfilesPath}" + "/scripts/.config/scripts/" + "jellyfin-mpv-shim-config-chose.sh";
   };
 
   xdg.configFile."jellyfin-mpv-shim/mpv.conf" = {
     source = "${config.values.dotfilesPath}" + "/jellyfin-mpv-shim/.config/jellyfin-mpv-shim/mpv.conf";
   };
-
 
   home.sessionPath = [ (config.xdg.configHome + "/scripts") ];
 }
