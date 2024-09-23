102b7925376a1c5f2566120c565821102c21c52d
rafal <you@example.com>
2024-09-23 13:29:22 +0200

    json lsp server

--- home/emacs.nix
+++ home/emacs.nix
@@ -6,6 +6,7 @@
 
     nixfmt-rfc-style # for nix mode in emacs
     bash-language-server # for sh mode lsp
+    nodePackages_latest.vscode-json-languageserver # lsp server for json
 
     # nerdfonts # NOTE: idk if it will solve fonts.
   ];
