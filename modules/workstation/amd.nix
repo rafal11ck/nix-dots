36a190db49dd69bf6bdcee6bde1bd4c973ad805a
rafal <you@example.com>
2024-09-23 18:13:46 +0200

    amd gpu setup

--- modules/workstation/amd.nix
+++ modules/workstation/amd.nix
@@ -6,5 +6,22 @@
 }:
 
 {
-  hardware.amdgpu.initrd.enable = lib.mkDefault true;
+  boot.initrd.kernelModules = [ "amdgpu" ];
+
+  hardware = {
+    amdgpu.initrd.enable = lib.mkDefault true;
+
+    opengl = {
+      driSupport32Bit = true; # Turn on vulcan for 32bit apps.
+
+      extraPackages = with pkgs; [
+        rocmPackages.clr.icd
+      ];
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    clinfo # To check openCL is setup correctly.
+  ];
+
 }
