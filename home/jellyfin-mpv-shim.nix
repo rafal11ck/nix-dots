509b449a30ab110dea64eaa12350f6070ee66ff0
rafal <you@example.com>
2024-09-23 13:38:58 +0200

    Use external mpv to get SVP working in mpv-shim.

--- home/jellyfin-mpv-shim.nix
+++ home/jellyfin-mpv-shim.nix
@@ -12,4 +12,9 @@
   xdg.configFile."jellyfin-mpv-shim/mpv.conf" = {
     source = "${config.values.dotfilesPath}" + "/jellyfin-mpv-shim/.config/jellyfin-mpv-shim/mpv.conf";
   };
+
+  xdg.configFile."jellyfin-mpv-shim/conf.json" = {
+    source = "${config.values.dotfilesPath}" + "/jellyfin-mpv-shim/.config/jellyfin-mpv-shim/conf.conf";
+  };
+
 }
