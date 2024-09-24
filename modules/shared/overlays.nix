c1e56e59f87bdce1bed095fb96c8b53577a87060
rafal <you@example.com>
2024-09-24 14:22:32 +0200

    add note for svp overlay

--- modules/shared/overlays.nix
+++ modules/shared/overlays.nix
@@ -12,6 +12,8 @@ in
     (self: super: {
       mpv = self.svp.mpv;
 
+      # workaroud till PR is merged
+      # https://github.com/NixOS/nixpkgs/pull/344152
       svp = self.callPackage (pkgsPath + /svp/package.nix) { };
     })
   ];
