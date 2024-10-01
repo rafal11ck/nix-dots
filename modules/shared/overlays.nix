2ce96b7b723f7ce97349d83acbe505cdf49707e7
rafal <you@example.com>
2024-10-01 14:53:47 +0200

    nh faces

--- modules/shared/overlays.nix
+++ modules/shared/overlays.nix
@@ -15,6 +15,28 @@ in
       # workaroud till PR is merged
       # https://github.com/NixOS/nixpkgs/pull/344152
       svp = self.callPackage (pkgsPath + /svp/package.nix) { };
+
+      nix-output-monitor =
+        let
+          icons = {
+            "↑" = "f062";
+            "↓" = "f063";
+            "⏱" = "f520";
+            "⏵" = "f04b";
+            "✔" = "f00c";
+            "⏸" = "f04c";
+            "⚠" = "f071";
+            "∅" = "f1da";
+            "∑" = "f04a0";
+          };
+        in
+        super.nix-output-monitor.overrideAttrs {
+          postPatch = ''
+            substituteInPlace lib/NOM/Print.hs \
+              ${lib.concatLines (lib.mapAttrsToList (old: new: "--replace-fail '${old}' '\\x${new}' \\") icons)}
+          '';
+        };
+
     })
   ];
 }
