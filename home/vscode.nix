838667f89dd3755fc92a7b5ef3678e0eca8f7824
rafal <you@example.com>
2024-10-03 16:44:35 +0200

    vscode and format

--- home/vscode.nix
+++ home/vscode.nix
@@ -13,17 +13,42 @@ in
   programs.vscode = {
     enable = true;
     extensions = with pkgs; [
+      vscode-extensions.catppuccin.catppuccin-vsc
+      vscode-extensions.catppuccin.catppuccin-vsc-icons
+      vscode-extensions.vscodevim.vim
       vscode-extensions.dbaeumer.vscode-eslint
       vscode-extensions.esbenp.prettier-vscode
-      vscode-extensions.catppuccin.catppuccin-vsc
     ];
     mutableExtensionsDir = true;
     userSettings = {
-      "files.refactoring.autoSave" = true;
-      "alt-catppuccin.accentColor" = lib.toLower values.catppuccinAccent;
-      "prettier.singleQuote" = true;
       "[javascriptreact]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
-      "workbench.colorTheme" = "Catppuccin Mocha";
+      "alt-catppuccin.accentColor" = lib.toLower values.catppuccinAccent;
+      "files.refactoring.autoSave" = true;
+
+      "editor.formatOnSave" = true;
+
+      "nix.enableLanguageServer" = true;
+      "nix.serverPath" = "nil";
+      "terminal.integrated.fontFamily" = "Fira Code Nerd Mono";
+
+      "nix.serverSettings" = {
+        nil = {
+          formatting = {
+            command = [ "nixfmt" ];
+          };
+        };
+      };
+
+      "editor.lineNumbers" = "relative";
+
+      "prettier.singleQuote" = true;
+      "workbench.colorTheme" = "Catppuccin ${values.catppuccinFlavor}";
+      "workbench.iconTheme" = "catppuccin-${lib.toLower values.catppuccinFlavor}";
+
+      "vim.handleKeys" = {
+        "<C-p>" = false;
+      };
+
     };
   };
 }
