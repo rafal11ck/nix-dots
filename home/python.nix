92af1711b1179bf3a646f3372038cb06b3f797df
rafal <you@example.com>
2024-10-23 14:20:10 +0200

    rework

--- home/python.nix
+++ home/python.nix
@@ -4,11 +4,14 @@
 }:
 {
   home.packages = with pkgs; [
-    (python3.withPackages (python-pkgs: [
-      # select Python packages here
-      python-pkgs.numpy
-      python-pkgs.pygments
-      python-pkgs.isort
-    ]))
+    (python3.withPackages (
+      python-pkgs: with python-pkgs; [
+        # select Python packages here
+        numpy
+        pygments
+        isort
+        matplotlib
+      ]
+    ))
   ];
 }
