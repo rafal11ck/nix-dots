60dff4554a415a35d3ce16096e82d0f59cea7cf4
rafal <you@example.com>
2024-10-24 09:22:38 +0200

    mocha hyprland catppcin theme

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -35,14 +35,6 @@
     ];
   };
 
-  xdg.configFile."hypr/appsworkspaces.conf".source =
-    "${config.values.dotfilesPath}" + "/hypr/.config/hypr/appsworkspaces.conf";
-  xdg.configFile."hypr/hyprland.conf".source =
-    "${config.values.dotfilesPath}" + "/hypr/.config/hypr/hyprland.conf";
-  xdg.configFile."hypr/keybinds.conf".source =
-    "${config.values.dotfilesPath}" + "/hypr/.config/hypr/keybinds.conf";
-  xdg.configFile."hypr/env.conf".source =
-    "${config.values.dotfilesPath}" + "/hypr/.config/hypr/env.conf";
-  xdg.configFile."hypr/autorun.conf".source =
-    "${config.values.dotfilesPath}" + "/hypr/.config/hypr/autorun.conf";
+  xdg.configFile."hypr/".source =
+    "${config.values.dotfilesPath}" + "/hypr/.config/hypr/";
 }
