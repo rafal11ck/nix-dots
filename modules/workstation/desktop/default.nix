aa8e6eeb7956353d4d2a1828add3fe7e60d838ff
rafal <you@example.com>
2024-11-11 12:02:31 +0100

    try to get thumbnails on videos

--- modules/workstation/desktop/default.nix
+++ modules/workstation/desktop/default.nix
@@ -13,7 +13,10 @@
     ./wine.nix
   ];
 
-  services.gnome.sushi.enable = true;
+  services = {
+    gnome.sushi.enable = true;
+    tumbler.enable = true;
+  };
   programs = {
     nautilus-open-any-terminal = {
       enable = true;
