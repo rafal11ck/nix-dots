700f5a7546b78b1d80ffcee08d550c34d2bd85b4
rafal <you@example.com>
2024-12-02 16:28:46 +0100

    update flake, adjust fonts to new scheme

--- home/style/fonts.nix
+++ home/style/fonts.nix
@@ -9,15 +9,10 @@
   home.packages = with pkgs; [
     zlib # fix https://github.com/nix-community/home-manager/issues/703 in standalone hm
     font-awesome
-    noto-fonts
+    nerd-fonts.symbols-only
+    nerd-fonts.fira-code
+    nerd-fonts.droid-sans-mono
     noto-fonts-cjk-sans
     noto-fonts-lgc-plus
-    (nerdfonts.override {
-      fonts = [
-        "NerdFontsSymbolsOnly"
-        "FiraCode"
-        "DroidSansMono"
-      ];
-    })
   ];
 }
