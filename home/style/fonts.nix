e24d0bde0146eef1a01d110e52788f4c972497eb
rafal <you@example.com>
2025-02-06 11:22:35 +0100

    add roboto font

--- home/style/fonts.nix
+++ home/style/fonts.nix
@@ -7,12 +7,13 @@
 
 {
   home.packages = with pkgs; [
-    zlib # fix https://github.com/nix-community/home-manager/issues/703 in standalone hm
+    zlib # HACK https://github.com/nix-community/home-manager/issues/703 in standalone hm
     font-awesome
     nerd-fonts.symbols-only
     nerd-fonts.fira-code
     nerd-fonts.droid-sans-mono
     noto-fonts-cjk-sans
     noto-fonts-lgc-plus
+    nerd-fonts.roboto-mono
   ];
 }
