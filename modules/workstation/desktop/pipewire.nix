166d6620e57222be7d8e7c6836f855357f538517
rafal <you@example.com>
2025-02-14 23:07:18 +0100

    feat: input sound noise supression

--- modules/workstation/desktop/pipewire.nix
+++ modules/workstation/desktop/pipewire.nix
@@ -8,6 +8,8 @@
 {
   services.pipewire = {
     enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
     socketActivation = true;
     pulse = {
       enable = true;
@@ -18,5 +20,44 @@
     wireplumber = {
       enable = true;
     };
+
+    extraConfig.pipewire."99-input-denoising" = {
+      "context.modules" = [
+        {
+          "name" = "libpipewire-module-filter-chain";
+          "args" = {
+            "node.description" = "Noise Canceling source";
+            "media.name" = "Noise Canceling source";
+            "filter.graph" = {
+              "nodes" = [
+                {
+                  "type" = "ladspa";
+                  "name" = "rnnoise";
+                  "plugin" = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
+                  "label" = "noise_suppressor_mono";
+                  "control" = {
+                    "VAD Threshold (%)" = 50.0;
+                    "VAD Grace Period (ms)" = 200;
+                    "Retroactive VAD Grace (ms)" = 0;
+                  };
+                }
+              ];
+            };
+
+            "capture.props" = {
+              "node.name" = "effect_input.rnnoise";
+              "node.passive" = true;
+              "audio.rate" = 48000;
+            };
+            "playback.props" = {
+              "node.name" = "effect_output.rnnoise";
+              "media.class" = "Audio/Source";
+              "audio.rate" = 48000;
+            };
+          };
+        }
+      ];
+    };
+
   };
 }
