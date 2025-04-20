6a573249270af07e919ce479c327b012af18aeec
rafal <you@example.com>
2025-04-21 00:45:32 +0200

    feat: cephfs mount

--- modules/workstation/automount.nix
+++ modules/workstation/automount.nix
@@ -2,24 +2,20 @@
   config,
   ...
 }:
-let
-  values = config.values;
-in
 {
-
   fileSystems = {
-
-    # NOTE requires credentials to be provided (man mount.davfs)
-    "/media/safe" = {
-      device = "https://cloud.cursedman.xyz/remote.php/dav/files/rafal/safe";
-      fsType = "davfs";
+    # NOTE requires setup in /etc/ceph
+    "/media/ext" = {
+      device = "192.168.0.55:6789,192.168.0.56:6789,192.168.0.57:6789:/ext";
+      fsType = "ceph";
       options = [
         "x-systemd.automount"
-        "x-systemd.idle-timeout=60"
-        "user"
-        "uid=${values.mainUser}"
+        "x-systemd.idle-timeout=300"
         "noauto"
         "_netdev"
+        "fs=ceph-filesystem"
+        "name=${config.networking.hostName}"
+        "noatime"
       ];
     };
 
