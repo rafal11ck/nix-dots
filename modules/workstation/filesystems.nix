87aadb15b9649470aafdcae9f6862874e1f8bbf7
rafal <you@example.com>
2025-05-04 16:17:53 +0200

    feat: mount cephfs

--- modules/workstation/filesystems.nix
+++ modules/workstation/filesystems.nix
@@ -2,21 +2,24 @@
   config,
   ...
 }:
+let
+  cephfsMountGen = path: {
+    device = "192.168.0.55:6789,192.168.0.56:6789,192.168.0.57:6789:${path}";
+    fsType = "ceph";
+    options = [
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=300"
+      "noauto"
+      "_netdev"
+      "fs=ceph-filesystem"
+      "name=${config.networking.hostName}"
+      "noatime"
+    ];
+  };
+in
 {
   fileSystems = {
-    # NOTE requires setup in /etc/ceph
-    "/media/cephfs" = {
-      device = "192.168.0.55:6789,192.168.0.56:6789,192.168.0.57:6789:/";
-      fsType = "ceph";
-      options = [
-        "x-systemd.automount"
-        "x-systemd.idle-timeout=300"
-        "noauto"
-        "_netdev"
-        "fs=ceph-filesystem"
-        "name=${config.networking.hostName}"
-        "noatime"
-      ];
-    };
+    "/media/cephfs/ext" = cephfsMountGen "/ext";
+    "/media/cephfs/volumes" = cephfsMountGen "/volumes";
   };
 }
