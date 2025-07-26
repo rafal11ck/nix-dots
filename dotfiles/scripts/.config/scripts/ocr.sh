0afba038b5161a6e2a27812d6bbbe813ef199832
rafal <you@example.com>
2025-07-26 09:37:15 +0200

    feat: ocr script

---
+++ dotfiles/scripts/.config/scripts/ocr.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# select area, then language read text from image and put it in clipboard
+
+set -xeu
+
+# check for dependencies
+for cmd in tesseract tofi grim slurp; do
+    if ! command -v "$cmd" &>/dev/null; then
+        notify-send "OCR dependencies are missing" "Missing command: $cmd in script $0"
+        exit 1
+    fi
+done
+
+OCRLANG="$(tesseract --list-langs | tail +2 | tofi)" grim -g "$(slurp)" - | tesseract -l "$OCRLANG" stdin stdout | wl-copy
