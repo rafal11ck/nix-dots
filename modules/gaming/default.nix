7905a86b0f55e29a78ea119118858bcfe219f380
rafal <you@example.com>
2026-02-11 20:00:24 +0100

    feat: add flatpak

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -19,4 +19,13 @@
     nix-alien
     prismlauncher
   ];
+
+  # TODO https://github.com/gmodena/nix-flatpak
+  services.flatpak.enable = true;
+
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = "flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo";
+  };
 }
