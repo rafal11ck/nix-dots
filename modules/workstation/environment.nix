3b805a7c1ac431094817818319ecd100531080d6
rafal <you@example.com>
2026-03-23 22:25:31 +0100

    chore: add jetbrains.rider

--- modules/workstation/environment.nix
+++ modules/workstation/environment.nix
@@ -51,6 +51,7 @@
     jdk25
     jellyfin-mpv-shim
     jetbrains.idea
+    jetbrains.rider
     jq
     kdePackages.okular
     keepassxc
@@ -158,7 +159,7 @@
     (mpv.override {
       scripts = with mpvScripts; [
         mpris
-        mpv-cheatsheet-ng
+        mpv-cheatsheet
       ];
     })
 
