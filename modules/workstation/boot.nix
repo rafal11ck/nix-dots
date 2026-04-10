f151283f08c1f4f009ba111b30e687c55154be34
rafal <you@example.com>
2026-04-10 15:18:21 +0200

    chore: remove disallow of efi var install

--- modules/workstation/boot.nix
+++ modules/workstation/boot.nix
@@ -5,7 +5,7 @@
 {
   # disable default from shared.
   boot.loader.systemd-boot.enable = lib.mkForce false;
-  boot.loader.efi.canTouchEfiVariables = lib.mkForce false;
+
 
   boot.loader.grub = {
     #copyKernels = true;
