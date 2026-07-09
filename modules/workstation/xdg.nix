a19ef29b20e8fb32142d592a4bada8e5ac08df15
rafal <you@example.com>
2026-07-09 15:34:00 +0200

    chore: hyprland has own portal

--- modules/workstation/xdg.nix
+++ modules/workstation/xdg.nix
@@ -6,9 +6,8 @@
   xdg = {
     portal = {
       enable = true;
-      wlr.enable = true;
       extraPortals = [
-        pkgs.xdg-desktop-portal-wlr
+        pkgs.xdg-desktop-portal-hyprland
         pkgs.xdg-desktop-portal-gtk
       ];
       config.common.default = [
