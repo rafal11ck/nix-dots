0736494ffd1f7ba12fc52439065e9d9c9880cc88
rafal <you@example.com>
2026-08-17 20:19:42 +0200

    fix: make electron apps use dedicated cpu

--- hosts/pc01/default.nix
+++ hosts/pc01/default.nix
@@ -25,6 +25,10 @@
     rocmSupport = true;
   };
 
+  environment.sessionVariables = {
+    AQ_DRM_DEVICES = "/dev/dri/by-path/pci-0000:03:00.0-card";
+  };
+
   boot.initrd.kernelModules = [ "amdgpu" ];
   services = {
     ollama = {
